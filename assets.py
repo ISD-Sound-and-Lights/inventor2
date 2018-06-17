e4c24b60e62a294dcf0f6287c573b364f253618e
DyingEcho <theo.court@yahoo.com>
2018-06-17 18:49:48 +0200

    Login system

--- assets.py
+++ assets.py
@@ -1,6 +1,16 @@
+import os
 import os.path
 import cgi
 import cgitb
+import bcrypt
+from http import cookies as Cookie
+
+
+auth_info = {
+		'basic': (b'$2b$12$c66G90rtLuaiNPjaKL4vnupurXCVLq8nSRDn82RFZt0y1aBZrRj0.', b'$2b$12$c66G90rtLuaiNPjaKL4vnu'),
+		'standard': (b'$2b$12$8emOe.SX.wgaGeOnUAjIgei/e5B0mIVJrS11J5YZIuVQhiFD./xqy', b'$2b$12$8emOe.SX.wgaGeOnUAjIge'),
+		'admin': (b'$2b$12$RUchRgKH36lPdlr7nX5cxe7g5jziRugZ0KUdVNHlLI8YdezU7DSPa', b'$2b$12$RUchRgKH36lPdlr7nX5cxe')
+		}
 
 
 class Template:
@@ -20,3 +30,37 @@ class Template:
 
 	def __repr__(self):
 		return self.template, self.replacers,
+
+
+def authenticate(username, password, checkCookies=False):
+	if checkCookies and __checkLoginCookie(): return 1  # cookies were good, so no need to manually check
+
+	username = str(username)
+	password = str(password)
+
+	try:
+		correctpw = auth_info[username][0]
+		salt = auth_info[username][1]
+	except KeyError:  # we do the rest anyway to prevent guessing correct usernames based on processing time
+		correctpw = b'incorrect username...'
+		salt = bcrypt.gensalt()
+	hashedpw = bcrypt.hashpw(password.encode(), salt)
+
+	if correctpw == hashedpw:
+		return 2
+	else:
+		return 0
+
+
+def __checkLoginCookie():
+	if "HTTP_COOKIE" in os.environ:
+		cookieString = os.environ.get("HTTP_COOKIE")
+		cookie = Cookie.SimpleCookie()
+		cookie.load(cookieString)
+
+		username = cookie['username'].value
+		password = cookie['password'].value
+
+		return authenticate(username, password, checkCookies=False)
+	else:
+		return False
--- index.py
+++ index.py
@@ -1,9 +1,20 @@
 #!/usr/local/bin/python3
 from assets import *
+from http import cookies as Cookie
+
+LocalStorage = cgi.FieldStorage()
+authenticatedState = authenticate(LocalStorage.getvalue('username'), LocalStorage.getvalue('password'))
+if authenticatedState == 1:
+	loggedIn = True
+elif authenticatedState == 2:
+	print('Set-Cookie: username="{}";'.format(LocalStorage.getvalue('username')))
+	print('Set-Cookie: password="{}";'.format(LocalStorage.getvalue('password')))
+	loggedIn = True
+elif authenticatedState == 0:
+	loggedIn = False
 
 print("Content-Type: text/html;charset=utf-8\n")
 cgitb.enable()
 
-
-ErrorTemplate = Template('index', loginPretty='logged in as: root')
-print(str(ErrorTemplate))
+PageTemplate = Template('index', loginPretty='logged in as: root' if loggedIn else 'not logged in')
+print(str(PageTemplate))
