436c8386ee09684aedbef63baa01e38cf8aff928
DyingEcho <theo.court@yahoo.com>
2018-06-17 11:54:55 +0200

    Update .gitignore

---
+++ error/405.py
@@ -0,0 +1,12 @@
+#!/usr/local/bin/python3
+import os.path
+from sys import path as syspath
+syspath.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), ".."))
+from assets import *
+
+print("Content-Type: text/html;charset=utf-8\n")
+cgitb.enable()
+
+
+ErrorTemplate = Template('error', errorNumber=404, loginPretty='logged in as: root')
+print(str(ErrorTemplate))
